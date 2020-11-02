885af6310e85488e7530a76cd9c1d4aafb26f901
Kn0wn-Un <naikdarsh175@gmail.com>
2020-11-02 22:39:01 +0530

    Prettier

--- aiscript.js
+++ aiscript.js
@@ -1,63 +1,61 @@
-const ai = (()=>{
+const ai = (() => {
     //scores with corresponding values
     let scores = {
-        'X': -1,
-        'O': +1,
-        'Draw': 0 
-    }
+        X: -1,
+        O: +1,
+        Draw: 0,
+    };
     //apply minimax to all available positions to get the best move
-    const playAi = ()=>{
-        let curBoard 
-        if(gameBoard.gameLog.length === 0)
-        curBoard = ['', '', '', '', '', '', '', '', ''];
-        else
-        curBoard = gameBoard.gameLog[gameBoard.gameLog.length - 1];
+    const playAi = () => {
+        let curBoard;
+        if (gameBoard.gameLog.length === 0)
+            curBoard = ["", "", "", "", "", "", "", "", ""];
+        else curBoard = gameBoard.gameLog[gameBoard.gameLog.length - 1];
         let tBoard = [...curBoard];
         let bestMove;
         let bestScore = -Infinity;
-        for(let i = 0; i < 9; i++){
-            if(tBoard[i] === ''){
-                tBoard[i] = 'O';
+        for (let i = 0; i < 9; i++) {
+            if (tBoard[i] === "") {
+                tBoard[i] = "O";
                 let score = minimax(tBoard, false);
-                tBoard[i] = '';
-                if(score > bestScore){
+                tBoard[i] = "";
+                if (score > bestScore) {
                     bestScore = score;
                     bestMove = i;
                 }
             }
         }
         playerO.play(bestMove);
-    }
+    };
     //minimax algorithm for all possible remaining moves
-    const minimax = (board, isMax)=>{
+    const minimax = (board, isMax) => {
         let result = game.checkWinner(board);
-        if(result){
+        if (result) {
             return scores[result];
         }
-        if(isMax){
+        if (isMax) {
             let bestScore = -Infinity;
-            for(let i = 0; i < 9; i++){
-                if(board[i] === ''){
-                    board[i] = 'O';
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === "") {
+                    board[i] = "O";
                     let score = minimax(board, false);
-                    board[i] = '';
+                    board[i] = "";
                     bestScore = Math.max(score, bestScore);
                 }
             }
             return bestScore;
-        }
-        else{
+        } else {
             let bestScore = Infinity;
-            for(let i = 0; i < 9; i++){
-                if(board[i] === ''){
-                    board[i] = 'X';
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === "") {
+                    board[i] = "X";
                     let score = minimax(board, true);
-                    board[i] = '';
+                    board[i] = "";
                     bestScore = Math.min(score, bestScore);
                 }
             }
             return bestScore;
         }
-    }
-    return {playAi }
+    };
+    return { playAi };
 })();
--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 //player object
-const Player = (name, symbol) => { 
-    const play = (btn_id) =>{
+const Player = (name, symbol) => {
+    const play = (btn_id) => {
         let btn = document.getElementById(btn_id);
         btn.innerHTML = symbol;
     };
-    return {name, symbol, play}
+    return { name, symbol, play };
 };
 
 //make two players
-let playerX = Player("Player X", "X");;
-let playerO = Player("Player O", "O");;
-
-
+let playerX = Player("Player X", "X");
+let playerO = Player("Player O", "O");
 
 //game module to keep track of moves and player
-const game = (()=>{
+const game = (() => {
     let curPlaying;
     const winPos = [
         [0, 1, 2],
@@ -24,83 +22,78 @@ const game = (()=>{
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6],
     ];
     //check if move is valid
-    const checkMove = (id) =>{
-        if(gameBoard.gameLog.length === 0)
-            return true
+    const checkMove = (id) => {
+        if (gameBoard.gameLog.length === 0) return true;
         let lastMove = gameBoard.gameLog[gameBoard.gameLog.length - 1];
-        if(lastMove[id] != "")
-            return false;
+        if (lastMove[id] != "") return false;
         return true;
-    } 
+    };
     //check if move is valid and not a winning move
-    const letPlay = (id) =>{
-        if(!checkMove(id))
-            return;
+    const letPlay = (id) => {
+        if (!checkMove(id)) return;
         playerX.play(id);
         logBoard();
-        if(checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])){
-            gameBoard.dispWinner(checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1]));
+        if (checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])) {
+            gameBoard.dispWinner(
+                checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])
+            );
             return;
         }
         ai.playAi();
         logBoard();
-        if(checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])){
-            gameBoard.dispWinner(checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1]));
+        if (checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])) {
+            gameBoard.dispWinner(
+                checkWinner(gameBoard.gameLog[gameBoard.gameLog.length - 1])
+            );
             return;
         }
     };
     //if move valid lod the board
-    const logBoard = () =>{
+    const logBoard = () => {
         let board = [];
-        for(let i = 0; i < 9; i++){
+        for (let i = 0; i < 9; i++) {
             let btn = document.getElementById(i);
             board[i] = btn.innerHTML;
         }
         gameBoard.gameLog.push(board);
-    }
+    };
     //reset board
-    const start = () =>{
+    const start = () => {
         gameBoard.gameLog = [];
-    }
+    };
     //check for winner
-    const checkWinner = (tBoard) =>{
+    const checkWinner = (tBoard) => {
         let X = getLength("X", tBoard);
         let O = getLength("O", tBoard);
-        for(let i = 0; i < winPos.length; i++){
-            if(arrayCheck("X", winPos[i], tBoard))
-                return "X";
-            else if(arrayCheck("O", winPos[i], tBoard))
-                return "O";
+        for (let i = 0; i < winPos.length; i++) {
+            if (arrayCheck("X", winPos[i], tBoard)) return "X";
+            else if (arrayCheck("O", winPos[i], tBoard)) return "O";
         }
-        if(X + O === 9)
-            return "Draw";
-        return false
-    }
+        if (X + O === 9) return "Draw";
+        return false;
+    };
     //get the positions of X and O
-    const getLength = (sym, board) =>{
+    const getLength = (sym, board) => {
         let tArr = [];
-        for(let i = 0; i < board.length; i++)
-            if(board[i] === sym)
-                tArr.push(i);
+        for (let i = 0; i < board.length; i++)
+            if (board[i] === sym) tArr.push(i);
         return tArr.length;
-    }
+    };
     //check if the positions of X or O are in corresponding winning positions
-    const arrayCheck = (sym, win, board) =>{
-        for(let i = 0; i < win.length; i++)
-            if(board[win[i]] != sym) return false;
+    const arrayCheck = (sym, win, board) => {
+        for (let i = 0; i < win.length; i++)
+            if (board[win[i]] != sym) return false;
         return true;
-    }
-    
-    return { start, letPlay, checkWinner, curPlaying }
+    };
+
+    return { start, letPlay, checkWinner, curPlaying };
 })();
 
-
-
 //game board module to keep track of game and other display elements
-const gameBoard = (()=>{
+const gameBoard = (() => {
     const gameLog = [];
     const board = document.querySelector(".game-board");
     const winDisp = document.querySelector(".winner");
@@ -115,16 +108,18 @@ const gameBoard = (()=>{
         board.style.visibility = "visible";
         board.classList.add("on-visible-board");
         btns.style.visibility = "visible";
-        setTimeout(function(){board.classList.remove("on-visible-board");}, 2000);
+        setTimeout(function () {
+            board.classList.remove("on-visible-board");
+        }, 2000);
         game.start();
-        for(let i = 1, id = 0; i <= 3; i++){
+        for (let i = 1, id = 0; i <= 3; i++) {
             let col = document.createElement("div");
-            col.classList.add("col"+i);
-            for(let j = 1; j <= 3; j++, id++){
+            col.classList.add("col" + i);
+            for (let j = 1; j <= 3; j++, id++) {
                 let play = document.createElement("div");
                 play.classList.add("play");
                 play.id = id;
-                play.addEventListener("click", ()=>game.letPlay(play.id));
+                play.addEventListener("click", () => game.letPlay(play.id));
                 col.appendChild(play);
             }
             board.appendChild(col);
@@ -135,33 +130,38 @@ const gameBoard = (()=>{
         change.addEventListener("click", dispForm);
     };
     //user name input
-    const dispForm = () =>{
+    const dispForm = () => {
         let name1 = prompt("Enter Player 1 Name", "PlayerX");
-        if(name1 != null) playerX.name = name1;
-        //let name2 = prompt("Enter Player 2 Name", "PlayerO");
-        //if(name2 != null) playerO.name = name2;
+        if (name1 != null) playerX.name = name1;
         setRand();
-    }
+    };
     //handling displaying the winner / draw
     const dispWinner = (winner) => {
-        board.style.visibility = "hidden"; 
-        if(winner === "Draw")
-            win.innerHTML = "Game is a draw";
+        board.style.visibility = "hidden";
+        if (winner === "Draw") win.innerHTML = "Game is a draw";
         else
-            win.innerHTML = "Winner is " +  (winner === playerX.symbol ? playerX.name : playerO.name);
+            win.innerHTML =
+                "Winner is " +
+                (winner === playerX.symbol ? playerX.name : playerO.name);
         winDisp.style.visibility = "visible";
-        winDisp.classList.add("on-visible-winner"); 
-        setTimeout(function(){btns.classList.add("on-winner-btn");}, 1250); 
-    }
+        winDisp.classList.add("on-visible-winner");
+        setTimeout(function () {
+            btns.classList.add("on-winner-btn");
+        }, 1250);
+    };
     //randomly set the starting player
-    const setRand = ()=>{
+    const setRand = () => {
         let dispCur = document.querySelector(".player-disp");
-        game.curPlaying = !(Math.floor(Math.random() * 2)) ? playerX.symbol : playerO.symbol;
-        dispCur.innerHTML = game.curPlaying === playerX.symbol? playerX.name + ", you are starting": "AI has started";
+        game.curPlaying = !Math.floor(Math.random() * 2)
+            ? playerX.symbol
+            : playerO.symbol;
+        dispCur.innerHTML =
+            game.curPlaying === playerX.symbol
+                ? playerX.name + ", you are starting"
+                : "AI has started";
         initialise();
-        if(game.curPlaying === playerO.symbol)
-            ai.playAi();
-    }
+        if (game.curPlaying === playerO.symbol) ai.playAi();
+    };
     return { gameLog, dispWinner, dispForm };
 })();
 gameBoard.dispForm();
